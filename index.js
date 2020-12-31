5bfd4ac6482f2ec8ace0cb44d3d5d8d20ddf91c1
Azara <64339787+AzaraUnknown@users.noreply.github.com>
2020-12-31 00:54:34 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const discord = require("discord.js")
+const bot = new discord.client();
+const token = NzM5MTg0MjIxMTE3MDg3ODY1.XyWw8w.oArVH4oNX8QAjvcJ-k4nIHWc-9I;
+
+
+bot.login(NzM5MTg0MjIxMTE3MDg3ODY1.XyWw8w.oArVH4oNX8QAjvcJ-k4nIHWc-9I);
